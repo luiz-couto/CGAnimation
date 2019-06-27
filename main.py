d4e74c705763ac4e9a2abd45d8f3ec014f3f2eef
Luiz <luiz.felipe.556@outlook.com>
2019-06-27 19:03:52 -0300

    Added LoadSkin function

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from OpenGL.GL import *
 import sys
 import struct
+from PIL import Image
+import numpy
 
 m_vertices      = []
 m_glcmds        = 0
@@ -112,6 +114,35 @@ def LoadModel(filename):
     print(m_vertices[78][0])
     #print(frame.verts[5].v)
 
+def LoadSkin(filename):
+    # this function: credits to http://www.magikcode.com/?p=122
+    # PIL can open BMP, EPS, FIG, IM, JPEG, MSP, PCX, PNG, PPM
+    # and other file types.  We convert into a texture using GL.
+    print('trying to open', filename)
+    try:
+        image = Image.open(filename)
+    except IOError as ex:
+        print('IOError: failed to open texture file')
+        message = template.format(type(ex).__name__, ex.args)
+        print(message)
+        return -1
+    print('opened file: size=', image.size, 'format=', image.format)
+    imageData = numpy.array(list(image.getdata()), numpy.uint8)
+
+    textureID = glGenTextures(1)
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 4)
+    glBindTexture(GL_TEXTURE_2D, textureID)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0)
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image.size[0], image.size[1],
+        0, GL_RGB, GL_UNSIGNED_BYTE, imageData)
+
+    image.close()
+    return textureID
+
 def main():
     LoadModel("Weapon.md2")
+    global m_texid
+    m_texid = LoadSkin("Weapon.pcx")
+    
 main()
